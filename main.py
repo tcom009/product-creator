169e71860ac6e32e2b8750e5140d44fc39130b1d
Gerson Mayer <g.mayerduran@gmail.com>
2022-04-14 01:16:45 -0500

    Swaped to bullet for cli interface

--- main.py
+++ main.py
@@ -1,38 +1,12 @@
-import functools
-from operator import add
-from simple_term_menu import TerminalMenu
-from functools import reduce
-from src.functions import get_labels
-
-# main_menu = [
-#     {"label": "> Añadir nuevo producto", "value": "CREATE_PRODUCT"},
-#     {"label": "> Salir", "value": "EXIT"}
-# ]
-
-
-# menu_labels = (reduce(lambda accumulator, value: accumulator +
-#                       [value["label"]], main_menu, []))
-
-# the letter in the square brackets too can use to index the options
-menu_labels = ['Añadir nuevo producto', 'Salir']
-
-
-def add_product_name():
-
-    product_name = input('Nombre')
+from random import choices
+from bullet import Bullet, Check, YesNo, Input  # and etc...
+# Create a Bullet or Check object
+choices = ["Create product", "Exit"]
+cli = Bullet(prompt="Choose from the items below: ",
+             choices=choices)
+result = cli.launch()
+if str(result) == choices[0]:
+    product_name = input('Product name: ')
     print(product_name)
-    show_menu()
-
-
-def show_menu():
-    choice = menu_labels[TerminalMenu(
-        menu_labels, title="Product creator", accept_keys=("y")).show()]
-
-    if choice == 'Añadir nuevo producto':
-        add_product_name()
-    elif choice == menu_labels[1]:
-        quit()
-
-
-if __name__ == '__main__':
-    show_menu()
+elif result == choices[1]:
+    quit()
