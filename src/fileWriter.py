e942132a292cc8b7dc8063e539f073ee65000683
Gerson Mayer <g.mayerduran@gmail.com>
2022-04-15 11:51:31 -0500

    adding write funtion

--- src/fileWriter.py
+++ src/fileWriter.py
@@ -1,3 +1,10 @@
+import json
+
 def writeFile(data):
-    for key, value in data.items():
-        print(key, ": ", value)
+    json_data = json.dumps(data, indent=4)
+    with open("products.json", "r") as openfile:
+        prev_data = json.load(openfile)
+        prev_data = json.dumps(prev_data, indent=4)
+        new_data = [prev_data].append(json_data)
+    with open("products.json", "w") as outfile:
+        outfile.write(json.dumps(new_data))
