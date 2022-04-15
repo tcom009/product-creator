3eaa588b77a20f53bb9fe0f615cb77f509ec8e1b
Gerson Mayer <g.mayerduran@gmail.com>
2022-04-14 19:02:47 -0500

    Create Product menu done

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from random import choices
+from src.mainMenu import main_menu
 from bullet import Bullet, Check, YesNo, Input  # and etc...
 # Create a Bullet or Check object
-choices = ["Create product", "Exit"]
-cli = Bullet(prompt="Choose from the items below: ",
-             choices=choices)
-result = cli.launch()
-if str(result) == choices[0]:
-    product_name = input('Product name: ')
-    print(product_name)
-elif result == choices[1]:
-    quit()
+
+
+def main():
+    main_menu()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/productMenu.py
@@ -0,0 +1,23 @@
+from bullet import Input, Numbers  # and etc...
+# Create a Bullet or Check object
+shipping_costs = {
+    "local": 0,
+    "provinces": 0
+}
+
+new_product = {
+    "shipping_costs": shipping_costs
+}
+
+
+def product_menu():
+    new_product["name"] = Input('Product name: ').launch()
+    new_product["price"] = Numbers('Product unit price: ').launch()
+    new_product["description"] = Input('Product decription: ').launch()
+    shipping_costs["local"] = Numbers('Local shipping cost: ').launch()
+    shipping_costs["provinces"] = Numbers('Provinces shipping cost: ').launch()
+
+
+def printItems():
+    for key, value in new_product.items():
+        print(key, ": ", value)
