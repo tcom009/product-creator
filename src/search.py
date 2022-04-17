133f34fd26f1944344b4a2ed53c18cef622d9a1b
Gerson Mayer <g.mayerduran@gmail.com>
2022-04-17 13:38:40 -0500

    Improved functions

--- src/search.py
+++ src/search.py
@@ -5,32 +5,42 @@ from src.fileWriter import read_file
 import re
 from bullet import Bullet
 
-options = []
-
-
-def getValues(result):
-    for option in options:
-        if option == result:
-            return option
-
 
 def searchData(name):
+
     file = read_file()
     file = pd.json_normalize(file)
     data_frame = pd.DataFrame(file)
-    casevalues = data_frame[data_frame.name.str.contains(
+    search_results = data_frame[data_frame.name.str.contains(
         name, flags=re.IGNORECASE)].values
-    for case in casevalues:
-        options.append(case[0])
-    menu = Bullet(choices=options)
-    bullet_result = menu.launch()
-    id = ""
-    for case in casevalues:
-        if case[0] == bullet_result:
-            id = case[3]
-    if bullet_result == getValues(bullet_result):
-        edit(id)
+    # fill option list
 
+    def getId(product_name, product_list):
+        id = ""
+        for product in product_list:
+            if product[0] == product_name:
+                id = product[3]
+        return id
+
+    def generate_options(result_values):
+        options = []
+        for result in result_values:
+            options.append(result[0])
+        return options
+
+    options = generate_options(search_results)
+
+    def getValues(result):
+        if result in options:
+
+            return result
+
+    menu = Bullet(choices=options)
+    # result from selection
+    bullet_result = menu.launch()
+
+    if bullet_result == getValues(bullet_result):
+        edit(getId(bullet_result, search_results))
 
 
 def showData(product_name):
