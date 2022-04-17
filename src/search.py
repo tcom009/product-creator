1688beab5ed01275a075f27f740afb3f61af7135
Gerson Mayer <g.mayerduran@gmail.com>
2022-04-17 12:49:56 -0500

    Added search without case sensitve using pandas and converting it to options for bullet object

--- src/search.py
+++ src/search.py
@@ -1,19 +1,29 @@
-
 import numpy as np
 import pandas as pd
 from bullet import Input
 from src.fileWriter import read_file
+import re
+from bullet import Bullet
 
 
 def searchData(name):
     file = read_file()
     file = pd.json_normalize(file)
     data_frame = pd.DataFrame(file)
-    print(data_frame[data_frame["name"] == name])
+    casevalues = data_frame[data_frame.name.str.contains(
+        name, flags=re.IGNORECASE)].values
+    options = []
+    for result in casevalues:
+        options.append(result[0])
+    menu = Bullet(choices=options)
+    result = menu.launch()
+
+    # print(type(casevalues))
+    # print(casevalues)
 
 
 def showData(product_name):
-    print(searchData(product_name))
+    searchData(product_name)
 
 
 def searchMenu():
