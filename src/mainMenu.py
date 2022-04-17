dbe1279a7736d79f612d2148d234bd21bf9708e2
Gerson Mayer <g.mayerduran@gmail.com>
2022-04-17 00:25:37 -0500

    added a simple search using name parameter

--- src/mainMenu.py
+++ src/mainMenu.py
@@ -1,6 +1,6 @@
 from bullet import Bullet
 from src.productMenu import product_menu
-from src.search import showData
+from src.search import searchMenu
 main_menu_choices = ["Create product",
                      "Edit Product", "Remove Product", "Exit"]
 main_menu = Bullet(prompt="Choose from the items below: ",
@@ -13,7 +13,7 @@ def main_menu():
         product_menu()
 
     elif result == main_menu_choices[1]:
-        showData()
+        searchMenu()
     elif result == main_menu_choices[2]:
         print(result)
 
--- src/search.py
+++ src/search.py
@@ -1,10 +1,21 @@
+
 import numpy as np
 import pandas as pd
+from bullet import Input
 from src.fileWriter import read_file
-file = read_file()
-
-data_frame = pd.DataFrame(file)
 
 
-def showData():
-    print(data_frame)
+def searchData(name):
+    file = read_file()
+    file = pd.json_normalize(file)
+    data_frame = pd.DataFrame(file)
+    print(data_frame[data_frame["name"] == name])
+
+
+def showData(product_name):
+    print(searchData(product_name))
+
+
+def searchMenu():
+    name = Input("Product name: ").launch()
+    showData(name)
